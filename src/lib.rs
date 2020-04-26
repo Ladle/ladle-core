1e1f68f4d721ecb39294a68142cb8ada5818b2d1
Kyle B <kylebrw@gmail.com>
2020-04-26 01:21:24 -0400

    changed repository purpose

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,1 @@
-/// The text module, which contains tools for storing texts
-/// taking spans of them and formatting span information nicely
-pub mod text;
-/// The trees module, which contains tree data structures
-pub mod trees;
-/// The parsers module, which contains parser algorithms
-/// and associated types
-pub mod parsers;
+mod ladle_toml;
